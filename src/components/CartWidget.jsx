c7f093e47576403ae219e9c8bb90df4583413913
Christian Adrian Bernal <christian.adrian.bernal@gmail.com>
2022-09-12 20:03:34 -0300

    cartcpntext

--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -3,7 +3,7 @@ import { ShoppingCartOutlined } from "@material-ui/icons";
 
 const CartWidget = () => {
     return (
-        <Badge badgeContent={2} color="secondary">
+        <Badge badgeContent={1} color="secondary">
             <ShoppingCartOutlined />
         </Badge>
     );
